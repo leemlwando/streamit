cab60660bbed249caa5bee1eba65448f67352124
leemlwando <leemlwando@gmail.com>
2018-11-20 19:36:25 +0200

    added client routes

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -6,6 +6,7 @@ const setup = require("./config");
 const path = require("path");
 
 const APIRoutes = require('./api'); //import api routes
+const ClientRoutes = require("./routes");
 
 setup.serviceConfig.mixins.push(ApiService); //create global broker object
 setup.serviceConfig.settings.use.push(APIRoutes); //register routes
@@ -19,7 +20,9 @@ app.set("views",path.join(__dirname,"views")); //set views path
 app.use(cors()); //set cross origin request headers
 app.use(bodyParser.json()); //parse json bodies
 app.use(bodyParser.urlencoded({extended:false}));
+app.use("/",ClientRoutes);
 app.use("/api/v1",svc.express()); //register broker
+
 app.use("/static",express.static(path.join(__dirname,"static"))); //set dir for static files
 
 app.use(setup.catch404); // catch 404 errors
